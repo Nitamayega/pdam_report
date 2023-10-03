16a6b9cd48f236a5ad8df42f1cc9d45a39c2193e
Nitamayega <ntmygaa@gmail.com>
2023-10-03 21:47:16 +0700

    Feat: Add Login Func

---
+++ app/src/main/java/com/pdam/report/ui/common/LoginActivity.kt
@@ -0,0 +1,65 @@
+package com.pdam.report.ui.common
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.pdam.report.MainActivity
+import com.pdam.report.R
+import com.pdam.report.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+    }
+
+    private fun setupAction() {
+        binding.buttonLogin.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val isEmailEmpty = email.isEmpty()
+            val password = binding.passwordEditText.text.toString()
+            val isPasswordEmpty = password.isEmpty()
+
+            binding.emailEditText.error = if (isEmailEmpty) getString(R.string.empty_field) else null
+            binding.passwordEditText.error = if (isPasswordEmpty) getString(R.string.empty_field) else null
+
+            if (!isEmailEmpty && !isPasswordEmpty) {
+                showLoading(true)
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    private fun setupView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+}
