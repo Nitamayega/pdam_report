e4990863a40f1a588741c9db2bb1abb566409cab
krafime <krafime@gmail.com>
2023-10-04 13:05:07 +0700

    feat: add splashscreen

--- app/src/main/java/com/pdam/report/MainActivity.kt
+++ app/src/main/java/com/pdam/report/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.pdam.report
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
