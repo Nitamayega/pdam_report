fafe5ebfb7a544dfa5ad4d276ce23c7301fdb2a9
Nitamayega <ntmygaa@gmail.com>
2023-10-26 19:53:50 +0700

    fix xmlView

--- app/src/main/java/com/pdam/report/ui/officer/PemasanganGPSActivity.kt
+++ app/src/main/java/com/pdam/report/ui/officer/PemasanganGPSActivity.kt
@@ -36,7 +36,6 @@ import java.io.File
 class PemasanganGPSActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityPemasanganGpsBinding.inflate(layoutInflater) }
-    private var imageFile: File? = null
 
     private val databaseReference = FirebaseDatabase.getInstance().reference
 
@@ -45,6 +44,11 @@ class PemasanganGPSActivity : AppCompatActivity() {
 
     private val userManager by lazy { UserManager() }
     private lateinit var user: UserData
+
+    private var imageNumber: Int = 0
+    private var firstImageFile: File? = null
+    private var secondImageFile: File? = null
+    private var thirdImageFile: File? = null
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
@@ -76,7 +80,9 @@ class PemasanganGPSActivity : AppCompatActivity() {
     }
 
     private fun setupButtons() {
-        binding.itemImage.setOnClickListener { startTakePhoto() }
+        binding.itemImage1.setOnClickListener { imageNumber = 1; startTakePhoto() }
+        binding.itemImage2.setOnClickListener { imageNumber = 2; startTakePhoto() }
+        binding.itemImage3.setOnClickListener { imageNumber = 3; startTakePhoto() }
         binding.btnSimpan.setOnClickListener { saveData() }
         binding.btnHapus.setOnClickListener {
             if (customerData == 2) {
@@ -127,35 +133,50 @@ class PemasanganGPSActivity : AppCompatActivity() {
             uploadImagesAndSaveData(user.username, currentDate, koorX, koorY, koorZ)
         } else {
             showLoading(false, binding.progressBar, binding.btnSimpan, binding.btnHapus)
-            showToast(this, R.string.fill_all_data)
+            showToast(this, R.string.fill_all_dataImage)
         }
     }
 
     private fun isInputValid(koorX: String, koorY: String, koorZ: String): Boolean {
         // Check if all required input is valid
-        return koorX.isNotEmpty() && koorY.isNotEmpty() && koorZ.isNotEmpty() && imageFile != null
+        return koorX.isNotEmpty() && koorY.isNotEmpty() && koorZ.isNotEmpty() && (firstImageFile != null) && (secondImageFile != null) && (thirdImageFile != null)
     }
 
     private fun uploadImagesAndSaveData(petugas: String, currentDate: Long, koorX: String, koorY: String, koorZ: String) {
         val storageReference = FirebaseStorage.getInstance().reference
-        val dokumentasiRef = storageReference.child("dokumentasi/${System.currentTimeMillis()}_dokumentasi5_GPS.jpg")
+        val dokumentasi3Ref = storageReference.child("dokumentasi/${System.currentTimeMillis()}_dokumentasi3_konstruksi.jpg")
+        val dokumentasi4Ref = storageReference.child("dokumentasi/${System.currentTimeMillis()}_dokumentasi4_meter.jpg")
+        val dokumentasi5Ref = storageReference.child("dokumentasi/${System.currentTimeMillis()}_dokumentasi5_perspektif.jpg")
+
 
         lifecycleScope.launch {
-            imageFile = imageFile?.reduceFileImageInBackground()
+            firstImageFile = firstImageFile?.reduceFileImageInBackground()
+            secondImageFile = secondImageFile?.reduceFileImageInBackground()
+            thirdImageFile = thirdImageFile?.reduceFileImageInBackground()
         }
 
-        // Upload image 4
-        dokumentasiRef.putFile(Uri.fromFile(imageFile)).addOnSuccessListener {
-            dokumentasiRef.downloadUrl.addOnSuccessListener { uri1 ->
-                val dokumentasi = uri1.toString()
+        dokumentasi3Ref.putFile(Uri.fromFile(firstImageFile)).addOnSuccessListener {
+            dokumentasi3Ref.downloadUrl.addOnSuccessListener { uri1 ->
+                val dokumentasi3 = uri1.toString()
 
-                // After successfully obtaining image URLs, save the data to Firebase
-                saveCustomerData(petugas, currentDate, koorX, koorY, koorZ, dokumentasi)
+                dokumentasi4Ref.putFile(Uri.fromFile(secondImageFile)).addOnSuccessListener {
+                    dokumentasi4Ref.downloadUrl.addOnSuccessListener { uri2 ->
+                        val dokumentasi4 = uri2.toString()
+
+                        dokumentasi5Ref.putFile(Uri.fromFile(thirdImageFile)).addOnSuccessListener {
+                            dokumentasi5Ref.downloadUrl.addOnSuccessListener { uri3 ->
+                                val dokumentasi5 = uri3.toString()
+
+                                saveCustomerData(petugas, currentDate, koorX, koorY, koorZ, dokumentasi3, dokumentasi4, dokumentasi5)
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
-    private fun saveCustomerData(petugas: String, currentDate: Long, koorX: String, koorY: String, koorZ: String, dokumentasi: String) {
+    private fun saveCustomerData(petugas: String, currentDate: Long, koorX: String, koorY: String, koorZ: String, dokumentasi3: String, dokumentasi4: String, dokumentasi5: String) {
         val customerRef = databaseReference.child("listCustomer").child(firebaseKey.toString())
 
         val data = mapOf(
@@ -164,7 +185,9 @@ class PemasanganGPSActivity : AppCompatActivity() {
             "xkoordinat" to koorX,
             "ykoordinat" to koorY,
             "zkoordinat" to koorZ,
-            "dokumentasi5" to dokumentasi,
+            "dokumentasi3" to dokumentasi3,
+            "dokumentasi4" to dokumentasi4,
+            "dokumentasi5" to dokumentasi5,
             "data" to 3
         )
 
@@ -229,8 +252,19 @@ class PemasanganGPSActivity : AppCompatActivity() {
             // After successfully capturing an image, assign it to the appropriate file
             val myFile = File(currentPhotoPath)
             myFile.let { file ->
-                imageFile = file
-                binding.itemImage.text = System.currentTimeMillis().toString() + "_dokumentasi5_GPS.jpg"
+                if (imageNumber == 1) {
+                    firstImageFile = file
+                    binding.itemImage1.text =
+                        System.currentTimeMillis().toString() + "_konstruksi.jpg"
+                } else if (imageNumber == 2) {
+                    secondImageFile = file
+                    binding.itemImage2.text =
+                        System.currentTimeMillis().toString() + "_meter.jpg"
+                } else if (imageNumber == 3) {
+                    thirdImageFile = file
+                    binding.itemImage2.text =
+                        System.currentTimeMillis().toString() + "_perspektif.jpg"
+                }
             }
         }
     }
@@ -266,6 +300,54 @@ class PemasanganGPSActivity : AppCompatActivity() {
 
         binding.edtAlamatPelanggan.apply {
             setText(dataCustomer.address)
+            isEnabled = false
+            isFocusable = false
+        }
+
+        binding.edtRt.apply {
+            setText(dataCustomer.rt)
+            isEnabled = false
+            isFocusable = false
+        }
+
+        binding.edtRw.apply {
+            setText(dataCustomer.rw)
+            isEnabled = false
+            isFocusable = false
+        }
+
+        binding.edtKelurahan.apply {
+            setText(dataCustomer.kelurahan)
+            isEnabled = false
+            isFocusable = false
+        }
+
+        binding.edtKecamatan.apply {
+            setText(dataCustomer.kecamatan)
+            isEnabled = false
+            isFocusable = false
+        }
+
+        binding.edtRt.apply {
+            setText(dataCustomer.rt)
+            isEnabled = false
+            isFocusable = false
+        }
+
+        binding.edtRw.apply {
+            setText(dataCustomer.rw)
+            isEnabled = false
+            isFocusable = false
+        }
+
+        binding.edtKelurahan.apply {
+            setText(dataCustomer.kelurahan)
+            isEnabled = false
+            isFocusable = false
+        }
+
+        binding.edtKecamatan.apply {
+            setText(dataCustomer.kecamatan)
             isEnabled = false
             isFocusable = false
         }
@@ -322,11 +404,31 @@ class PemasanganGPSActivity : AppCompatActivity() {
             isFocusable = false
         }
 
-        binding.itemImage.apply {
+        binding.itemImage1.apply {
+            text = parsingNameImage(dataCustomer.dokumentasi3)
+            setOnClickListener {
+                supportFragmentManager.beginTransaction()
+                    .add(FullScreenImageDialogFragment(dataCustomer.dokumentasi1), "FullScreenImageDialogFragment")
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+
+        binding.itemImage2.apply {
+            text = parsingNameImage(dataCustomer.dokumentasi4)
+            setOnClickListener {
+                supportFragmentManager.beginTransaction()
+                    .add(FullScreenImageDialogFragment(dataCustomer.dokumentasi2), "FullScreenImageDialogFragment")
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+
+        binding.itemImage3.apply {
             text = parsingNameImage(dataCustomer.dokumentasi5)
             setOnClickListener {
                 supportFragmentManager.beginTransaction()
-                    .add(FullScreenImageDialogFragment(dataCustomer.dokumentasi5), "FullScreenImageDialogFragment")
+                    .add(FullScreenImageDialogFragment(dataCustomer.dokumentasi3), "FullScreenImageDialogFragment")
                     .addToBackStack(null)
                     .commit()
             }
